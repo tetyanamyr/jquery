830533f971d4488d8a631eeaabfc76937a2c22a9
tetyanamyr <tetyana.myronova@ssense.com>
2018-12-10 10:26:04 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+$("#loaderDiv").hide();
+    $("#hide").click(function(){
+        $("#loaderDiv").hide();
+
+    });
+    $("#submit-button").click(function(){
+        $("#loaderDiv").show();
+
+    });
+});
